abeb949086d159b59460bd81a63be47a091fac5a
Tushar Prajapati <tusharprt5@gmail.com>
2025-08-11 00:44:18 +0530

    error

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,6 +1,7 @@
 import Course from "../ models/courseModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
+import { GoogleGenAI } from "@google/genai";
 
 const getCourseDetails = asyncHandler(async(req,res)=>{
     try {
@@ -34,4 +35,42 @@ const createCourse = asyncHandler(async(req,res)=>{
     }
 });
 
-export {getCourseDetails, createCourse}
+const summarizePdf = asyncHandler(async(req,res)=>{
+        try {
+            const {pdfUrl} = req.body;
+            // if(!pdfUrl){
+            //     throw new ApiError(402, "No PDF Found!")
+            // }
+            // const pdf = await fetch(pdfUrl);
+            // console.log(pdf);
+
+            // if (!pdf.ok) {
+            //     throw new ApiError(402, "Unable to fetch PDF");
+            // }
+
+            // const pdfBuffer = await pdf.arrayBuffer();
+            // const pdfData = await PdfParse(Buffer.from(pdfBuffer));
+            // const extractedText = pdfData.text;
+            // if (!extractedText.trim()) {
+            //      throw new ApiError(402, "No text found in PDF") ;
+            // }
+            // const prompt = `Summarize the following text in a concise paragraph:\n\n${extractedText}`;
+            // res.status(200).send(prompt);
+            
+            
+
+
+            const ai = new GoogleGenAI({});
+            
+            // const response = await ai.models.generateContent({
+            //     model: "gemini-2.5-flash",
+            //     contents: "Explain how AI works in a few words",
+            //   });
+            //   res.status(200).json({res: response.text});
+
+        } catch (error) {
+            throw new ApiError(403, error)
+        }
+})
+
+export {getCourseDetails, createCourse, summarizePdf}
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getCourseDetails, createCourse } from '../controllers/courseController.js';
+import { getCourseDetails, createCourse, summarizePdf } from '../controllers/courseController.js';
 
 const router = express.Router();
 
 router.route('/:courseId').get(getCourseDetails);
 router.route('/create').post(createCourse);
-
+router.route('/summarize').post(summarizePdf);
 export default router;
